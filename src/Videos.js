1dda7d628b40c1f747ea4415b80e11f43e06cedc
Vinod Phalke <vinodcphalke@gmail.com>
2023-03-21 16:12:11 +0530

    First Commit

---
+++ src/Videos.js
@@ -0,0 +1,149 @@
+import React from "react";
+import "./Videos.css";
+import Filter from "./Filter";
+import SingleVideo from "./SingleVideos";
+
+function Videos() {
+  return (
+    <div className="videos">
+      <Filter />
+      <div className="row">
+       <SingleVideo
+          video_link="https://youtu.be/C2sE8N6DwTg"
+          thumb_img="https://i.ytimg.com/vi/C2sE8N6DwTg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCvCzykX7-EjKz_xDyrHHWenJMt9g"
+          profile_img="https://yt3.ggpht.com/ytc/AL5GRJVednFKQGzbHz7qk4NfZ7HrWS7twAoK8fMYXdy-=s68-c-k-c0x00ffffff-no-rj"
+          title="How To Become FEARLESS? || 3 Hard Realities"
+          channel_name="Being Krishna Conscious"
+          video_duration="00:08:19"
+        />
+         <SingleVideo
+          video_link="https://youtu.be/_wzJnWCBWkI"
+          thumb_img="https://i.ytimg.com/vi/_wzJnWCBWkI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD_BTfVwuBzdy5AK9zD9RrHKXlUJA"
+          profile_img="https://yt3.ggpht.com/OLch8c2hBL4sUyASo11VsmCpOJxHrKpheJi_moNyRl4Fq8Cz6KQL6_bYWd8_Twz1iEkC_YuyCHw=s68-c-k-c0x00ffffff-no-rj"
+          title="You're Not Slow: Become a Speed Learner in 20 Minutes"
+          channel_name="Elizabeth Filips"
+          video_duration="00:19:18"
+        />
+         <SingleVideo
+          video_link="https://youtu.be/-0X0BHLE0Dg"
+          thumb_img="https://i.ytimg.com/vi/-0X0BHLE0Dg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAHtq8Mwdy0JeQlZdRObdNXosdqfw"
+          profile_img="https://yt3.ggpht.com/ytc/AL5GRJVednFKQGzbHz7qk4NfZ7HrWS7twAoK8fMYXdy-=s68-c-k-c0x00ffffff-no-rj"
+          title="How to setup Visual Studio Code for Java | Java Extension for visual studio code | Coding Wallah"
+          channel_name="Coding Wallah"
+          video_duration="00:45:22"
+        />         
+         <SingleVideo
+          video_url="https://youtu.be/d6RsKunbWME"
+          thumb_img="https://i.ytimg.com/vi/d6RsKunbWME/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBXCCmvWXESHudxEgFP0SNgmR1dAg"
+          profile_img="https://yt3.ggpht.com/ytc/AL5GRJXzF8Sw35Uu3CWzStubrLSPSePClJj6FkGqVPs3DQ=s68-c-k-c0x00ffffff-no-rj"
+          title="M2 MacBook Pro 14 One Week Later! A Shocking Update?!"
+          channel_name="The Everyday Dad"
+          video_duration="00:40:09"
+        />         
+      </div>
+      <div className="row">
+      <SingleVideo
+          video_url="https://youtu.be/onKs9lXLa5Q"
+          thumb_img="https://i.ytimg.com/vi/onKs9lXLa5Q/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA0Mxi0_Mkmdf9FwOVaeLX9plhYCQ"
+          profile_img="https://yt3.ggpht.com/ytc/AL5GRJWBKXtbOq_ZkV6sNjg1M2G3ctNVGquFP6_131Cn=s68-c-k-c0x00ffffff-no-rj"
+          title="THE 2023 G-WAGON 4x4 SQUARED IS HERE! || Manny Khoshbin"
+          channel_name="Manny Khoshbin"
+          video_duration="00:08:15"
+        />
+         <SingleVideo
+          video_url="https://youtu.be/MugtkoKJYWg"
+          thumb_img="https://i.ytimg.com/vi/MugtkoKJYWg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDISHo09sstJfKrrjXlmWcJ4_v0SA"
+          profile_img="https://yt3.ggpht.com/3dhST2suIfX5hbfWZixmdxRBUDyGx01B-_IE3wqBp2tBjgBpBWbF-m5rltC_RbWEq4_2pqlV=s68-c-k-c0x00ffffff-no-rj"
+          title="Watch this before you get the M2 Max Macbook Pro for coding ..."
+          channel_name="Engineering with Utsav"
+          video_duration="00:17:15"
+        />
+         <SingleVideo
+          video_url="https://youtu.be/4y3h5Lsyick"
+          thumb_img="https://i.ytimg.com/vi/4y3h5Lsyick/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA00ky3vA3pKy_GiC3-BD56xCTI_g"
+          profile_img="https://yt3.ggpht.com/jrW0ubvtsXYvHrmqJonw96f1q7FUpR_hkN-QYxP653YiwvJqxQbNbEgoeVTn7eW5JToLUwA-Wg=s68-c-k-c0x00ffffff-no-rj"
+          title="NEW 2023 Audi A9 Quattro Avant Luxury V8 700hp- FIRST LOOK 4k"
+          channel_name="ROYAL CAR"
+          video_duration="00:03:22"
+        />
+         <SingleVideo
+          video_url="https://youtu.be/MGURQpNVed0"
+          thumb_img="https://i.ytimg.com/vi/MGURQpNVed0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA1Jqmw98lj9vB8tmu0QuSFQyWVwQ"
+          profile_img="https://yt3.ggpht.com/3dhST2suIfX5hbfWZixmdxRBUDyGx01B-_IE3wqBp2tBjgBpBWbF-m5rltC_RbWEq4_2pqlV=s68-c-k-c0x00ffffff-no-rj"
+          title="$700 000 REZVANI VENGEANCE - 810 HP! Test drive and full review. Road legal military truck!"
+          channel_name="alan_drives"
+          video_duration="00:13:03"
+        />
+      </div>
+      <div className="row">
+      <SingleVideo
+          video_url="https://youtu.be/onKs9lXLa5Q"
+          thumb_img="https://i.ytimg.com/vi/onKs9lXLa5Q/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA0Mxi0_Mkmdf9FwOVaeLX9plhYCQ"
+          profile_img="https://yt3.ggpht.com/ytc/AL5GRJWBKXtbOq_ZkV6sNjg1M2G3ctNVGquFP6_131Cn=s68-c-k-c0x00ffffff-no-rj"
+          title="THE 2023 G-WAGON 4x4 SQUARED IS HERE! || Manny Khoshbin"
+          channel_name="Manny Khoshbin"
+          video_duration="00:08:15"
+        />
+         <SingleVideo
+          video_url="https://youtu.be/MugtkoKJYWg"
+          thumb_img="https://i.ytimg.com/vi/MugtkoKJYWg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDISHo09sstJfKrrjXlmWcJ4_v0SA"
+          profile_img="https://yt3.ggpht.com/3dhST2suIfX5hbfWZixmdxRBUDyGx01B-_IE3wqBp2tBjgBpBWbF-m5rltC_RbWEq4_2pqlV=s68-c-k-c0x00ffffff-no-rj"
+          title="Watch this before you get the M2 Max Macbook Pro for coding ..."
+          channel_name="Engineering with Utsav"
+          video_duration="00:17:15"
+        />
+         <SingleVideo
+          video_url="https://youtu.be/4y3h5Lsyick"
+          thumb_img="https://i.ytimg.com/vi/4y3h5Lsyick/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA00ky3vA3pKy_GiC3-BD56xCTI_g"
+          profile_img="https://yt3.ggpht.com/jrW0ubvtsXYvHrmqJonw96f1q7FUpR_hkN-QYxP653YiwvJqxQbNbEgoeVTn7eW5JToLUwA-Wg=s68-c-k-c0x00ffffff-no-rj"
+          title="NEW 2023 Audi A9 Quattro Avant Luxury V8 700hp- FIRST LOOK 4k"
+          channel_name="ROYAL CAR"
+          video_duration="00:03:22"
+        />
+         <SingleVideo
+          video_url="https://youtu.be/MGURQpNVed0"
+          thumb_img="https://i.ytimg.com/vi/MGURQpNVed0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA1Jqmw98lj9vB8tmu0QuSFQyWVwQ"
+          profile_img="https://yt3.ggpht.com/3dhST2suIfX5hbfWZixmdxRBUDyGx01B-_IE3wqBp2tBjgBpBWbF-m5rltC_RbWEq4_2pqlV=s68-c-k-c0x00ffffff-no-rj"
+          title="$700 000 REZVANI VENGEANCE - 810 HP! Test drive and full review. Road legal military truck!"
+          channel_name="alan_drives"
+          video_duration="00:13:03"
+        />
+      </div>
+      <div className="row">
+      <SingleVideo
+          video_url="https://youtu.be/onKs9lXLa5Q"
+          thumb_img="https://i.ytimg.com/vi/onKs9lXLa5Q/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA0Mxi0_Mkmdf9FwOVaeLX9plhYCQ"
+          profile_img="https://yt3.ggpht.com/ytc/AL5GRJWBKXtbOq_ZkV6sNjg1M2G3ctNVGquFP6_131Cn=s68-c-k-c0x00ffffff-no-rj"
+          title="THE 2023 G-WAGON 4x4 SQUARED IS HERE! || Manny Khoshbin"
+          channel_name="Manny Khoshbin"
+          video_duration="00:08:15"
+        />
+         <SingleVideo
+          video_url="https://youtu.be/MugtkoKJYWg"
+          thumb_img="https://i.ytimg.com/vi/MugtkoKJYWg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDISHo09sstJfKrrjXlmWcJ4_v0SA"
+          profile_img="https://yt3.ggpht.com/3dhST2suIfX5hbfWZixmdxRBUDyGx01B-_IE3wqBp2tBjgBpBWbF-m5rltC_RbWEq4_2pqlV=s68-c-k-c0x00ffffff-no-rj"
+          title="Watch this before you get the M2 Max Macbook Pro for coding ..."
+          channel_name="Engineering with Utsav"
+          video_duration="00:17:15"
+        />
+         <SingleVideo
+          video_url="https://youtu.be/4y3h5Lsyick"
+          thumb_img="https://i.ytimg.com/vi/4y3h5Lsyick/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA00ky3vA3pKy_GiC3-BD56xCTI_g"
+          profile_img="https://yt3.ggpht.com/jrW0ubvtsXYvHrmqJonw96f1q7FUpR_hkN-QYxP653YiwvJqxQbNbEgoeVTn7eW5JToLUwA-Wg=s68-c-k-c0x00ffffff-no-rj"
+          title="NEW 2023 Audi A9 Quattro Avant Luxury V8 700hp- FIRST LOOK 4k"
+          channel_name="ROYAL CAR"
+          video_duration="00:03:22"
+        />
+         <SingleVideo
+          video_url="https://youtu.be/MGURQpNVed0"
+          thumb_img="https://i.ytimg.com/vi/MGURQpNVed0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA1Jqmw98lj9vB8tmu0QuSFQyWVwQ"
+          profile_img="https://yt3.ggpht.com/3dhST2suIfX5hbfWZixmdxRBUDyGx01B-_IE3wqBp2tBjgBpBWbF-m5rltC_RbWEq4_2pqlV=s68-c-k-c0x00ffffff-no-rj"
+          title="$700 000 REZVANI VENGEANCE - 810 HP! Test drive and full review. Road legal military truck!"
+          channel_name="alan_drives"
+          video_duration="00:13:03"
+        />
+      </div>
+    </div>
+  );
+}
+export default Videos;
